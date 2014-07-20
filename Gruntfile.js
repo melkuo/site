c945b1b3ec98dba82974ac8cb80719426f426a4b
melodyk <melody.kuo@live.com>
2014-07-19 23:06:42 -0400

    Put everything into a src folder + updated grunt file to copy to temp and dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,30 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		sass: {                              // Task
-			dist: {                            // Target
-				options: {                       // Target options
+		paths: {
+			dist: 'dist',
+			src: 'src',
+			temp: 'temp',
+			css: 'css',
+			fonts: 'fonts',
+			images: 'img',
+			js: 'js'
+		},
+
+		clean: {
+			all: ['<%= paths.temp %>', '<%= paths.dist %>'],
+			temp: ['<%= paths.temp %>']
+		},
+
+		sass: {
+			temp: {
+				options: {
 					compass: 'true'
 				},
 				files:[{
 					expand: true,
-					cwd: 'sass',
+					cwd: '<%= paths.src %>/sass',
 					src: '**/*.{css,scss}',
-					dest: 'css',
+					dest: '<%= paths.temp %>/css',
 					ext: '.css'
 				}]
 			}
@@ -17,12 +32,68 @@ module.exports = function(grunt) {
 
 		watch: {
 			sass: {
-				files: ['sass/**/*.{css,scss}',],
+				files: ['<%= paths.src %>/sass/**/*.{css,scss}',],
 				tasks: ['sass']
+			}
+		},
+
+		copy: {
+			temp: {
+				files: [{
+					expand: true,
+					cwd: '<%= paths.src %>',
+					src: [
+						'<%= paths.css %>/*.css',
+						'<%= paths.fonts %>/*.{eot,svg,ttf,woff}',
+						'<%= paths.js %>/*.js',
+						'<%= paths.images %>/*.{gif,png,jpg,svg}',
+						'*.html'
+					],
+					dest: '<%= paths.temp %>'
+				}]
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= paths.src %>',
+					src: [
+						'<%= paths.css %>/*.css',
+						'<%= paths.fonts %>/*.{eot,svg,ttf,woff}',
+						'<%= paths.js %>/*.js',
+						'<%= paths.images %>/*.{gif,png,jpg,svg}',
+						'*.html'
+					],
+					dest: '<%= paths.dist %>'
+				}]
+			},
+			sass: {
+				files: [{
+					expand: true,
+					cwd: '<%= paths.temp %>',
+					src: [
+						'<%= paths.css %>/*.css'
+					],
+					dest: '<%= paths.dist %>'
+				}]
+			}
+		},
+
+		uglify: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= paths.src %>',
+					src: [
+						'<%= paths.js %>/*.js'
+					],
+					dest: '<%= paths.dist %>'
+				}]
 			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -32,7 +103,18 @@ module.exports = function(grunt) {
 	]);
 
 	grunt.registerTask('dev', [
+		'clean',
 		'sass',
+		'copy:temp',
 		'watch'
+	]);
+
+	grunt.registerTask('build', [
+		'clean',
+		'sass',
+		'copy:dist',
+		'copy:sass', //copy 'sass' from temp to dist
+		'uglify',
+		'clean:temp'
 	]);
 };
