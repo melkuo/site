62cc09354012b3d87e656806cd1624f5632fecdb
melodyk <melody.kuo@live.com>
2014-07-20 20:46:59 -0400

    Added more grunt tasks: connect, livereload and tweaked others

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,7 @@ module.exports = function(grunt) {
 		paths: {
 			dist: 'dist',
 			src: 'src',
-			temp: 'temp',
-			css: 'css',
-			fonts: 'fonts',
-			images: 'img',
-			js: 'js'
+			temp: 'temp'
 		},
 
 		clean: {
@@ -15,25 +11,16 @@ module.exports = function(grunt) {
 			temp: ['<%= paths.temp %>']
 		},
 
-		sass: {
-			temp: {
+		connect: {
+			options: {
+				port: '8000',
+				open: 'http://localhost:8000/index.html',
+				base: ['<%= paths.temp %>']
+			},
+			dev: {
 				options: {
-					compass: 'true'
-				},
-				files:[{
-					expand: true,
-					cwd: '<%= paths.src %>/sass',
-					src: '**/*.{css,scss}',
-					dest: '<%= paths.temp %>/css',
-					ext: '.css'
-				}]
-			}
-		},
-
-		watch: {
-			sass: {
-				files: ['<%= paths.src %>/sass/**/*.{css,scss}',],
-				tasks: ['sass']
+					livereload: true
+				}
 			}
 		},
 
@@ -43,10 +30,10 @@ module.exports = function(grunt) {
 					expand: true,
 					cwd: '<%= paths.src %>',
 					src: [
-						'<%= paths.css %>/*.css',
-						'<%= paths.fonts %>/*.{eot,svg,ttf,woff}',
-						'<%= paths.js %>/*.js',
-						'<%= paths.images %>/*.{gif,png,jpg,svg}',
+						'css/*.css',
+						'fonts/*.{eot,svg,ttf,woff}',
+						'js/*.js',
+						'img/*',
 						'*.html'
 					],
 					dest: '<%= paths.temp %>'
@@ -57,23 +44,56 @@ module.exports = function(grunt) {
 					expand: true,
 					cwd: '<%= paths.src %>',
 					src: [
-						'<%= paths.css %>/*.css',
-						'<%= paths.fonts %>/*.{eot,svg,ttf,woff}',
-						'<%= paths.js %>/*.js',
-						'<%= paths.images %>/*.{gif,png,jpg,svg}',
+						'css/*.css',
+						'fonts/*.{eot,svg,ttf,woff}',
+						'js/*.js',
+						'img/*',
 						'*.html'
 					],
 					dest: '<%= paths.dist %>'
 				}]
+			}
+		},
+
+		sass: {
+			options: {
+				compass: 'true'
 			},
-			sass: {
-				files: [{
+			temp: {	
+				options: {
+					style: 'expanded'
+				},
+				files:[{
 					expand: true,
-					cwd: '<%= paths.temp %>',
+					cwd: '<%= paths.src %>/sass',
+					src: '**/*.{css,scss}',
+					dest: '<%= paths.temp %>/css',
+					ext: '.css'
+				}]
+			},
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files:[{
+					expand: true,
+					cwd: '<%= paths.src %>/sass',
+					src: '**/*.{css,scss}',
+					dest: '<%= paths.dist %>/css',
+					ext: '.css'
+				}]
+			}
+		},
+
+		sync: {
+			temp: {
+				files: [{
+					cwd: '<%= paths.src %>',
 					src: [
-						'<%= paths.css %>/*.css'
+						'**',
+						'!sass/**/*.scss'
 					],
-					dest: '<%= paths.dist %>'
+					dest: '<%= paths.temp %>'
 				}]
 			}
 		},
@@ -84,37 +104,68 @@ module.exports = function(grunt) {
 					expand: true,
 					cwd: '<%= paths.src %>',
 					src: [
-						'<%= paths.js %>/*.js'
+						'js/*.js'
 					],
 					dest: '<%= paths.dist %>'
 				}]
+			}
+		},
+
+		watch: {
+			options: {
+				livereload: true
+			},
+			css: {
+				files: ['<%= paths.src %>/css/**/*.css'],
+				tasks: ['sync']
+			},
+			fonts: {
+				files: ['<%= paths.src %>/fonts/**/*.{eot,svg,ttf,woff}'],
+				tasks: ['sync']
+			},
+			img: {
+				files: ['<%= paths.src %>/img/**/*.{eot,svg,ttf,woff}'],
+				tasks: ['sync']
+			},
+			js: {
+				files: ['<%= paths.src %>/js/**/*.js'],
+				tasks: ['sync']
+			},
+			sass: {
+				files: ['<%= paths.src %>/sass/**/*.{css,scss}'],
+				tasks: ['sass:temp']
+			},
+			html: {
+				files: ['<%= paths.src %>/*.html'],
+				tasks: ['sync']
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sync');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', [
-		'sass'
+		'dev'
 	]);
 
 	grunt.registerTask('dev', [
-		'clean',
-		'sass',
-		'copy:temp',
-		'watch'
+		'clean',		// delete temp and dist directories
+		'sass:temp',	// compile sass and place in temp directory
+		'copy:temp',	// copy the rest of the files from src to temp
+		'connect:dev',	// connect to web server
+		'watch'			// watch for changes in files
 	]);
 
 	grunt.registerTask('build', [
-		'clean',
-		'sass',
-		'copy:dist',
-		'copy:sass', //copy 'sass' from temp to dist
-		'uglify',
-		'clean:temp'
+		'clean',		// delete temp and dist directories
+		'sass:dist',	// compile sass and place in dist directory
+		'copy:dist',	// copy the rest of the files from src to dist
+		'uglify'		// minify js
 	]);
 };
