d744489484929f52059d9077d704054dfb9e6a75
unknown <melody@melody.klick.com>
2014-07-24 11:40:55 -0400

    JS now detects user agent + only load a bunch of stuff on desktop

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,63 +1,95 @@
+/********** Detect Device **********/
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 $(document).ready(function() {
-	/********** Functions **********/
-	var parallaxScroll = function($bgobj) {
-		// scrollTop gets the current scroll value from the top i.e. how much the user has scrolled up
-		// bgobj.data('speed') refers to the data-speed you assigned in the html
-		// yPos = how much we scrolled up divided by data-speed. It's negative because we're going opposite direction of the user's scroll
-		// e.g. if user scrolls 50px down, background scrolls 5px up
-		if (mqBgrdImageBreak.matches) { // if width > 1024px
-			var yPos = -125 - ($window.scrollTop() / $bgobj.data('speed'));
-		} else {
-			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-		}
+	$('#home').css('height', $(window).height());
+
+	if (isMobile.any()) { 
+		// show/hide parts permanently on mobile
+		$('.bgDescLabel').hide();
+		$('.aboutMe').show();
+		$('.instructions').show();
+		$('.toPortfolio').hide();
+		$('.toHome').hide();		
+
+	} else { // if not mobile
+
+		/********** Functions **********/
+		var parallaxScroll = function($bgobj) {
+			// scrollTop gets the current scroll value from the top i.e. how much the user has scrolled up
+			// bgobj.data('speed') refers to the data-speed you assigned in the html
+			// yPos = how much we scrolled up divided by data-speed. It's negative because we're going opposite direction of the user's scroll
+			// e.g. if user scrolls 50px down, background scrolls 5px up
+			if ($(window).width() > 1400) {
+				var yPos = -125 - ($window.scrollTop() / $bgobj.data('speed'));
+			} else {
+				var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			}
+			
+			// Put together our final background position
+			// 50% as its xPosition to keep horizontal position static and center
+			var coords = '50% ' + yPos + 'px';
+
+			// Move the background
+			$bgobj.css({backgroundPosition: coords});
+		};
+
 		
-		// Put together our final background position
-		// 50% as its xPosition to keep horizontal position static and center
-		var coords = '50% ' + yPos + 'px';
+		/********** Media Queries **********/
+		var mqBgrdImageBreak = window.matchMedia( "(min-width: 1400px)" );
+		
 
-		// Move the background
-		$bgobj.css({backgroundPosition: coords});
-	};
+		/********** Fade In On Load **********/
+		$('.aboutMe').hide();
+		$('nav').hide();
+		$('.bgDescLabel').hide();
+		$('.toPortfolio').hide();
 
-	
-	/********** Media Queries **********/
-	var mqSmall = window.matchMedia( "(min-width: 481px)" );
-	var mqBgrdImageBreak = window.matchMedia( "(min-width: 1400px)" );
-	
-
-	/********** Fade In On Load **********/
-	$('.aboutMe').hide();
-	$('nav').hide();
-	$('.bgDescLabel').hide();
-	$('.toPortfolio').hide();
-
-	$('.aboutMe').delay(300).fadeIn(900);
-	$('nav').delay(1300).fadeIn(1200);
-	$('.bgDescLabel').delay(1300).fadeIn(1200);
-	$('.toPortfolio').delay(1300).fadeIn(1200);
+		$('.aboutMe').delay(300).fadeIn(900);
+		$('nav').delay(1300).fadeIn(1200);
+		$('.bgDescLabel').delay(1300).fadeIn(1200);
+		$('.toPortfolio').delay(1300).fadeIn(1200);
 
 
-	/********** Parallex scroll **********/
-	// Cache the Window object
-	$window = $(window);
-	
-	$('section[data-type="background"]').each(function() {
-		var $bgobj = $(this); // assigning the object
+		/********** Parallex scroll **********/
+		// Cache the Window object
+		$window = $(window);
+		
+		$('section[data-type="background"]').each(function() {
+			var $bgobj = $(this); // assigning the object
 
-		$(window).scroll(function(){
-			parallaxScroll($bgobj);
-		});
+			$(window).scroll(function(){
+				parallaxScroll($bgobj);
+			});
 
-		$(window).resize(function() {
-			parallaxScroll($bgobj);
-		});
-	}); 
+			$(window).resize(function() {
+				parallaxScroll($bgobj);
+			});
+		}); 
 
 
-	/********** Sick Fadez **********/
-	$(window).scroll(function() {
-		// Fade background description label and abooutMe section in/out on scroll
-		if(mqSmall.matches) { 	// if width > 480px
+		/********** Sick Fadez While Scrolling **********/
+		$(window).scroll(function() {
+			// Fade background description label and abooutMe section in/out on scroll
 			// Parts from the bottom of home fade out early on
 			if ($(this).scrollTop() < 20) {
 				$('.bgDescLabel').fadeIn('fast');
@@ -87,42 +119,30 @@ $(document).ready(function() {
 			} else {
 				$('.toHome').fadeIn('fast');
 			}
-
-		} else {	// show/hide certain parts permanently on mobile
-			$('.bgDescLabel').hide();
-			$('.aboutMe').show();
-			$('.instructions').show();
-			$('.toPortfolio').hide();
-			$('.toHome').hide();
-		}
-
-		// Change colour of nav	for different sections
-		if(mqSmall.matches) { 	// if width > 480px
+				
+			// Change colour of nav	for different sections
 			if ($(this).scrollTop() < 950) {
 				$('nav').removeClass().addClass('lightColor');
 			} else {
 				$('nav').removeClass('lightColor').addClass('darkColor');
 			}
-		} else {
-			$('nav').removeClass('darkColor').addClass('lightColor');
-		}
-		
-	});
+		});
 
-	/********** Smooth Scrolling **********/
-	$('a').click(function() {
-		$('html, body').animate({
-			scrollTop: $( $.attr(this, 'href') ).offset().top
-		}, 900);
-		return false;
-	});
+		/********** Smooth Scrolling **********/
+		$('a').click(function() {
+			$('html, body').animate({
+				scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, 900);
+			return false;
+		});
 
-	/********** On hover, fade arrows **********/
-	$('.toHome, .toPortfolio').hover(function() {
-		$(this).fadeTo('fast', '0.5');
-	}, function() {
-		$(this).fadeTo('fast', '1');
-	});
+		/********** On hover, fade arrows **********/
+		$('.toHome, .toPortfolio').hover(function() {
+			$(this).fadeTo('fast', '0.5');
+		}, function() {
+			$(this).fadeTo('fast', '1');
+		});
+	}
 });
 
 /********** Create HTML5 Element for IE **********/
