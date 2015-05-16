442a0450e15cf770e2dd8f2515bd1255b507bc9d
melodyk <melody.kuo22@gmail.com>
2015-05-16 16:43:33 -0400

    Removed jQuery + added scripts for next/prev link + anchors open in new tab

---
+++ src/js/scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var order = ["connect", "tabbit", "contrave", "ttc", "robot", "thread", "skyline", "red"],
+    prevEl = document.querySelector("[role='prev-proj']"),
+    nextEl = document.querySelector("[role='next-proj']"),
+    path = (/\/([^]*)\.html/).exec(window.location.pathname)[1],
+      /* 0 = match, 1 = capture */
+    i = order.indexOf(path);
+
+  if (order[i-1]) {
+    prevEl.setAttribute("href", order[i-1].concat(".html"));
+    prevEl.classList.remove("hide");
+  }
+
+  if (order[i+1]){
+    nextEl.setAttribute("href", order[i+1].concat(".html"));
+    nextEl.classList.remove("hide");
+  }
+});
