cc3bf6045b08c04a0264d83649ecee3a1c8d1274
melodyk <melody.kuo22@gmail.com>
2016-01-09 23:44:01 -0500

    Updated look.
    * Added captions to thumbnails.
    * Re-arranged order or details and description on project page.
    * Changed font to PT Sans.
    * "Removed" art projects (didn't delete files).
    * And more.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var order = ["connect", "tabbit", "contrave", "robot", "thread", "red"],
+  var order = ["connect", "tabbit", "contrave"],
     prevEl = document.querySelector("[role='prev-proj']"),
     nextEl = document.querySelector("[role='next-proj']"),
     path = (/\/([^]*)\.html/).exec(window.location.pathname)[1],
       /* 0 = match, 1 = capture */
     i = order.indexOf(path);
+
+  document.querySelector("[role='to-index']").classList.remove("hide");
 
   if (order[i-1]) {
     prevEl.setAttribute("href", order[i-1].concat(".html"));
@@ -15,4 +17,5 @@ document.addEventListener('DOMContentLoaded', function() {
     nextEl.setAttribute("href", order[i+1].concat(".html"));
     nextEl.classList.remove("hide");
   }
+
 });
