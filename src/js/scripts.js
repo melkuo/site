01101e95dbaae4b1b3f495004f3f893aaea798a2
melodyk <melody.kuo22@gmail.com>
2016-06-11 16:44:46 -0400

    Added project name to next/prev. Updated copy for Docs.com.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,19 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
   var order = ["docscom", "connect", "tabbit", "beacon"],
     prevEl = document.querySelector("[role='prev-proj']"),
     nextEl = document.querySelector("[role='next-proj']"),
-    path = (/\/([^]*)\.html/).exec(window.location.pathname)[1],
-      /* 0 = match, 1 = capture */
+    path = (/\/([^]*)\.html/).exec(window.location.pathname)[1], /* 0 = match, 1 = capture */
     i = order.indexOf(path);
 
   document.querySelector("[role='to-index']").classList.remove("hide");
 
   if (order[i-1]) {
     prevEl.setAttribute("href", order[i-1].concat(".html"));
+    prevEl.appendChild(document.createTextNode("Back to " + cmap[order[i-1]]));
     prevEl.classList.remove("hide");
   }
 
-  if (order[i+1]){
+  if (order[i+1]) {
     nextEl.setAttribute("href", order[i+1].concat(".html"));
+    nextEl.appendChild(document.createTextNode("Next to " + cmap[order[i+1]]));
     nextEl.classList.remove("hide");
   }
 
